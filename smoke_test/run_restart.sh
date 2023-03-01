387e5c566b97cd2754dcec1ebcdd08f54aad25c5
Mike Anderson <anderson2981@gmail.com>
2023-02-28 21:03:08 -0600

    smoke_test_ks

--- smoke_test/run_restart.sh
+++ smoke_test/run_restart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpirun -n 2 python -u -O -m mpi4py prediction.py -i run_params.yaml -r restart_data/prediction-000000010 -t restart_data/prediction-000000000 --log
+mpirun -n 2 python -u -O -m mpi4py driver.py -i run_params.yaml -r restart_data/prediction-000000010 -t restart_data/prediction-000000000 --log
