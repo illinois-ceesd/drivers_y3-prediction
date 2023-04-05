11e8535019f0757fdd351c5cfe57d6d1c6ee2aed
Michael T. Campbell <mtcampbe@illinois.edu>
2023-04-04 20:21:11 -0700

    Use these settings for fine flame1d.

--- flash_test/flash_test.bsub.sh
+++ flash_test/flash_test.bsub.sh
@@ -2,14 +2,19 @@
 #BSUB -nnodes 1
 #BSUB -G uiuc
 #BSUB -W 360
-#BSUB -J flash10
+#BSUB -J flash9
 #BSUB -q pbatch
-#BSUB -o flash10.out
+#BSUB -o flash9.out
 
 source ../emirge/config/activate_env.sh
 source ../emirge/mirgecom/scripts/mirge-testing-env.sh
 
 set -x
-$MIRGE_MPI_EXEC -n 1 $MIRGE_PARALLEL_SPAWNER python -u -O -m mpi4py driver.py -c ${casename} -i run_params.yaml -r restart_data/prediction-001020000 --log --lazy
+$MIRGE_MPI_EXEC -n 1 $MIRGE_PARALLEL_SPAWNER python -u -O -m mpi4py driver.py -c ${casename} -i run_params.yaml -r restart_data/prediction-000965000 --log --lazy
 set +x
 
+# flash5 000483000
+# flash6 000604000
+# flash7 000726000
+# flash8 000847000
+# flash9 000965000
