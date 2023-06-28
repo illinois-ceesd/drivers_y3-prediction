9e7ab6b4a82a31d567eb12a2f522987ac620932f
Mike Anderson <anderson2981@gmail.com>
2023-06-28 09:32:11 -0500

    update mirgecom branch

--- buildMirge.sh
+++ buildMirge.sh
@@ -3,7 +3,7 @@
 # default branch for building mirgecom for this driver
 #mirge_branch="production"
 #mirge_branch="wall_model_exposed_gradients"
-mirge_branch="production-generalize-coupling"
+mirge_branch="production-generalize-coupling-mja"
 # conda environment name
 conda_env="mirgeDriver.Y3prediction"
 
