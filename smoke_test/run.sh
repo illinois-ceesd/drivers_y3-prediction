5830f81102677f210aba7f0c6fdac0aea701819c
Mike Anderson <anderson2981@gmail.com>
2023-09-06 10:07:48 -0500

    turn off -O to enable asserts in CI

--- smoke_test/run.sh
+++ smoke_test/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mpirun -n 2 python -u -O -m mpi4py driver.py -i run_params.yaml --log --casename=prediction-eager
-#mpirun -n 1 python -u -O -m mpi4py driver.py -i run_params.yaml --log
+#mpirun -n 2 python -u -O -m mpi4py driver.py -i run_params.yaml --log --casename=prediction-eager
+mpirun -n 2 python -u -m mpi4py driver.py -i run_params.yaml --casename=prediction-eager
