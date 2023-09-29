10669876df5f4103170e217b58df0ecfd32ba72e
eduardoma2 <em18@illinois.edu>
2023-09-29 13:35:08 -0500

    new oblique shock init case

--- smoke_test/run_esdg 2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpirun -n 2 python -u -O -m mpi4py driver.py --esdg --numpy -i run_params.yaml
--- smoke_test/run_numpy 2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpirun -n 2 python -u -O -m mpi4py driver.py -i run_params.yaml --log --numpy --casename=prediction-numpy
