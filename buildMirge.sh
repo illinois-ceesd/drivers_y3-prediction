67e903f4ea8d8fb5b23011235332eedf60504cfd
Mike Anderson <anderson2981@gmail.com>
2024-01-04 16:00:46 -0600

    support new limiter

--- buildMirge.sh
+++ buildMirge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # default branch for building mirgecom for this driver
-mirge_branch="production"
+mirge_branch="production_new_limit"
 # conda environment name
 conda_env="mirgeDriver.Y3prediction"
 
