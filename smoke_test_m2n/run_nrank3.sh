958bd9a4dbb47d0cfb102dd8cd51f565d2ee79b6
Mike Anderson <anderson2981@gmail.com>
2024-08-28 11:01:02 -0500

    oversubscribe m2n test

--- smoke_test_m2n/run_nrank3.sh
+++ smoke_test_m2n/run_nrank3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpirun -n 3 python -u -m mpi4py driver.py -i run_params.yaml --numpy --casename=prediction-nrank3
+mpirun -n 3 --oversubscribe python -u -m mpi4py driver.py -i run_params.yaml --numpy --casename=prediction-nrank3
