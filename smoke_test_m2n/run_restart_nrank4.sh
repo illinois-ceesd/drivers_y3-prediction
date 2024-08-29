a8d0b3f8d5df6e37a93580c98db50d5e016ca85e
anderson2981 <41346365+anderson2981@users.noreply.github.com>
2024-08-29 14:29:24 -0500

    Test m to n restart (#67)
    * smoke test for m2n restart
    
    * adding m2n smoke test to ci
    
    * point to m2n mirgecom branch
    
    * oversubscribe m2n test
    
    * missing stuff
    
    * update to production

---
+++ smoke_test_m2n/run_restart_nrank4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mpirun -n 4 --oversubscribe python -u -m mpi4py driver.py -i run_params.yaml -r restart_data_4p/prediction-nrank3-000000010 -t restart_data_4p/prediction-nrank3-000000000 --casename=prediction-restart-nrank4 --numpy
