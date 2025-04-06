0ed6259e7c92e5ff2524e7b4e34fdd212239fba9
Mike Campbell <mtcampbe@illinois.edu>
2025-04-06 13:07:31 -0500

    resolve axi radial velocity into 3d y,z components.

--- y3prediction/prediction.py
+++ y3prediction/prediction.py
@@ -5017,7 +5017,13 @@ def main(actx_class, restart_filename=None, target_filename=None,
 
         axi_cv = fluid_restart_items["cv"]
         axi_mom = axi_cv.momentum
-        restart_mom = make_obj_array([axi_mom[0], axi_mom[1], 0*axi_mom[0]])
+        fl_y = fluid_nodes[1]
+        fl_z = fluid_nodes[2]
+        fl_r = actx.np.sqrt(fl_y*fl_y + fl_z*fl_z)
+        xfer_mom_x = axi_mom[1]  # y-component of axi maps to Vx
+        xfer_mom_y = axi_mom[0] * fl_y/fl_r  # Vy_axi * r_hat_x
+        xfer_mom_z = axi_mom[0] * fl_z/fl_r  # Vy_axi * r_hat_y
+        restart_mom = make_obj_array([xfer_mom_x, xfer_mom_y, xfer_mom_z])
         restart_cv = axi_cv.replace(momentum=restart_mom)
 
         temperature_seed = fluid_restart_items["tseed"]
